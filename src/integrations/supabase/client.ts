8c64560e2cfad5512618741171217746d7cfc101
Joseph Amos <josephamos626@gmail.com>
2026-07-31 20:31:41 -0400

    add  identifiant files 2

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -2,13 +2,12 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL as string;
-const SUPABASE_PUBLISHABLE_KEY = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY as string;
-
+const VITE_SUPABASE_PUBLISHABLE_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF4YXVoanhqcXllc3ZxZHp4Ym5oIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjExNDczNTIsImV4cCI6MjA3NjcyMzM1Mn0.KWoIYTcVmI_X-2B4g5BhLnf75FktQrrV8LDZoHEr8-w"
+const VITE_SUPABASE_URL="https://qxauhjxjqyesvqdzxbnh.supabase.co"
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
 
-export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY, {
+export const supabase = createClient<Database>(VITE_SUPABASE_URL, VITE_SUPABASE_PUBLISHABLE_KEY, {
   auth: {
     storage: localStorage,
     persistSession: true,
